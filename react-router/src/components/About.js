7929f3dfdcbd31efa3c59a8373d11cc062d8fc6c
Abdulaziz Ali <guuree1982@gmail.com>
2022-08-10 21:24:50 -0400

    bugs fixed

--- react-router/src/components/About.js
+++ react-router/src/components/About.js
@@ -10,7 +10,7 @@ export default function About () {
             <p>As the heir of the industrious McMansions fortune, Monty was never satisfied with the material wealth that provided him everything he desired. </p>
             <p>Monty decided to travel around the world to find peace and happiness</p>
             <p>Upon his return, he dedicated about 1% of his wealth to open this Mineral SPA to bring joy and spiritual happiness to others</p>
-            <img src="hotsprings.jpg" alt="hotsprings" />I 
+            <img src="hotsprings.jpg" alt="hotsprings" />
         </div>
     )
 }
